1c1f06b526855963b32aa14d98da347d0e0ed6f1
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-09-09 19:42:41 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import PostForm from "./components/PostForm";
 import User from "./components/User";
 
+// pas de router ici , on a une seule page, tout se passe dans les components!
+// pr commencer blog (ou API Back) en Redux (ici React-Redux) ; reprendr le même précodage des Components ici;
+
+// avant de créer le Store (trés important en Redux); il faut créer un Provider qui englobe  notre App ; il faut faire ça dans Index.js
+
 const App = () => {
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
+import App from "./App";
+// import "./styles/index.scss";
+
+// import React
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import "./styles/index.scss";
+// import Redux
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+// ici import de rootReducer! JS de base va lire l'index.js dans dossier reducers
+import rootReducer from "./reducers";
+//-------------------------------------
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// d'abord avant tout en Redux et dans ce fichier (ici!!) créer le PROVIDER!! qui envelopera toute l'App; à faire ICI dans index.js! comme suite:
+// comme suite: " Provider store={store} " englobe l'app ; comme ci-dessou!
+
+// créer variable constante store ; elle contient méthode createStore (qui est méthode de Redux) ;
+// dans createStore y a le Reducer qui les regroupe tous; il s'appelle rootReducer (pr créer fichier reducer on va faire un fichier index.js ; qu'on va créer dans dossier reducers)
+
+// se que sera rootReducer est : combineReducers (la somme de tous nos reducers) qui va constituer NOTRE STORE !!!!!
+// on fait createStore avec rootReducer ;
+const store = createStore(
+  rootReducer,
+  //ce qui est en-bàs c pr utiliser devTools et nous aider à developper!
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
