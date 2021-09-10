4f174a2fe9efa757bd20ded8750e75ba9d440da2
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-09-10 18:51:14 +0300

    Add files via upload

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 //partie User (component) pr inserer une logique d'utilisateur (pseudo, photo, âge, nbr de likes)
 
 const User = () => {
+  const user = useSelector((state) => state.userReducer);
+
   return (
     <div className="user-container">
       <div className="user">
-        <h3>userPseudo</h3>
+        <h3>{user[0].pseudo}</h3>
         <img src="https://thispersondoesnotexist.com/image" alt="" />
         <p>Age : 35 ans</p>
         <p>Like(s) : 0</p>
