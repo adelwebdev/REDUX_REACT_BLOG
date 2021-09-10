574471601a13133dc7cc18fba8208ada7294e56c
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-09-10 18:39:02 +0300

    Add files via upload

--- src/reducers/post.reducer.js
+++ src/reducers/post.reducer.js
@@ -2,9 +2,25 @@
 // faire un initial state (de base) et un export default function avec 2 papamètres
 // les 2 params; state et action (tous le sreducers se présentent de cette façon!!!)
 // c dans le return que ça se passe! (ce que fait le reducer)
+import { GET_POSTS } from "../actions/post.action";
 
-const initialState = { stateDeBase: "hello" };
+// const initialState = { stateDeBase: "hello" };
+// export default function postReducer(state = initialState, action) {
+//   return initialState;
+// }      CA C'ETAIT AVANT QU'ON NE CREE LES ACTIONS
 
+// maintenant aprés création de la fct getPosts (fichier: post.action.js)
+const initialState = {};
+// dans notre reducer ; c là qu'on va créer le Switch!!
 export default function postReducer(state = initialState, action) {
-  return initialState;
+  switch (action.type) {
+    case GET_POSTS:
+      return action.payload;
+    default:
+      return state;
+  }
 }
+// en faisant action.payload: il prend tt la contenance de payload (notre data avec Get) et il va la mettre dans initial state
+// ensuite cette data sera regroupé dans index.js (tt les reducers) qui mènent à l'index.js et qui sont dans le Store
+// on a crée la fonction, on a crée l'action, l'action qui va chercher la data et qui l'exploite pr incrémenter notre Store
+// par contre on ne l'appelle pas encore; on veut la data soit dispo dès l'ouverture de l'App; on fait ça dans index.js racine (voir fichier)
--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -1,8 +1,19 @@
 // un autre reducer
 // post.reducer et user.reducer comme 2 sous-partie pr stocker le reducer
 // c avec ces 2 qu'on va créer les mouvements dynamique en Front-end!
+// ici aussi on utilise le switch de la même façon! avec switch (action.type)
+import { GET_USER } from "../actions/user.action";
+
 const initialState = {};
 
 export default function userReducer(state = initialState, action) {
-  return initialState;
+  switch (action.type) {
+    case GET_USER:
+      return action.Payload;
+    default:
+      return state;
+  }
 }
+// on return le state qui évolue en permanence, il passe de zéro (initialState = {}) à ex: action.payload ce qu'on obtien avec Get sur DB Users
+// le state sera égale à ce qu'il y a dans action.payload ensuite il évolura de façon dynamique
+// pr se le jouer dès que l'appli est lancé (même principe que avec db posts) voir : index.js
